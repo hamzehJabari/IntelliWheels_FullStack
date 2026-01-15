7a302dd28b04e0ccac6e4a010ddf4800d10aebf8
Hamza53Hazem <21110127@htu.edu.jo>
2026-01-15 19:18:14 +0300

    Update logo image source path for AppView component to relative path

--- src/components/AppView.tsx
+++ src/components/AppView.tsx
@@ -3104,8 +3104,8 @@ export function AppView() {
               onClick={() => setActivePage('listings')}
             >
               <img
-                src="C:\Users\hamze\Desktop\IntelliWheels_FullStack\IntelliWheels (3).png"
-                alt="IntelliWheels Logo"
+                src="/IntelliWheels (3).png"
+                alt="IntelliWheels"
                 className="h-20 w-auto object-contain object-center transition-transform group-hover:scale-105 drop-shadow-lg"
               />
             </div>
